40d9616122d4c5bf2d79807e6dce67f44c6df09f
moyoussef55 <gy20191000@gmail.com>
2026-08-03 03:38:31 +0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://btjjjsewlfyllatlasoy.supabase.co";
-
-const SUPABASE_KEY = "الصق هنا المفتاح اللي بدأ بـ sb_publishable_";
-
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
